7f3fa2fc351489f30d17add45317b131fade5572
BramLobbens <lobbensbram@gmail.com>
2024-03-30 18:00:19 +0100

    add ut

---
+++ auth_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/bootdotdev/learn-cicd-starter/internal/auth"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	testHeader := http.Header{
+		"Host":          {"www.host.com"},
+		"Content-Type":  {"application/json"},
+		"Authorization": {"ApiKey foo"},
+	}
+
+	gotRes, gotErr := auth.GetAPIKey(testHeader)
+	want := "foo"
+	if gotRes != want || gotErr != nil {
+		t.Errorf("got %q, wanted %q", gotRes, want)
+	}
+}
